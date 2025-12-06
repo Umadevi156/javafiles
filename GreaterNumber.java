e8314d7e65ab251527d489b0a4dd89dcdde724e7
Umadevi D S <umadevids2005@gmail.com>
2025-12-06 20:28:07 +0530

    Add files via upload

---
+++ GreaterNumber.java
@@ -0,0 +1,33 @@
+package java1;
+import java.util.Scanner;
+
+
+public class GreaterNumber {
+
+	public static void main (String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a:");
+		
+		int a = sc.nextInt();
+		System.out.println("Enter b:");
+		int b = sc.nextInt();
+		
+		
+		
+	if( a == 6 ||b == 6|| (a + b) == 6 || (a - b) == 6) {
+			System.out.println("true");
+		
+	}
+	else {
+		System.out.println("false");
+	}
+		   
+		
+		
+		
+		
+
+	}
+	}
+
